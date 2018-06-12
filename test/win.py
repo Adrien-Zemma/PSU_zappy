923bb9a32bb0c75eef601951f5873677e0ac2696
Adrien Zemma <adrien.zemma@epitech.eu>
2018-06-12 20:25:46 +0200

    fixing and working map

--- test/win.py
+++ test/win.py
@@ -5,21 +5,21 @@ from pygame.locals import *
 		
 
 def getMap():
-	return [[{"linemate":1, "deraumere":5, "sibur":2, "mendiane":1, "phiras":1, 'thystame':1},
-		{"linemate":1, "deraumere":5, "sibur":2, "mendiane":1, "phiras":1, 'thystame':1}],
-		[{"linemate":1, "deraumere":5, "sibur":2, "mendiane":1, "phiras":1, 'thystame':1},
-		{"linemate":1, "deraumere":5, "sibur":2, "mendiane":1, "phiras":1, 'thystame':1}]]
+	return [[{"linemate":0, "deraumere":1, "sibur":1, "mendiane":1, "phiras":1, 'thystame':1, "food":1},
+		{"linemate":1, "deraumere":1, "sibur":1, "mendiane":1, "phiras":1, 'thystame':1, "food":1}],
+		[{"linemate":1, "deraumere":1, "sibur":1, "mendiane":1, "phiras":1, 'thystame':1, "food":1},
+		{"linemate":1, "deraumere":1, "sibur":1, "mendiane":1, "phiras":1, 'thystame':1, "food":1}]]
 class Drawer():
 	def __init__(self, x, y):
 		random.seed()
-		pygame.init()
 		self._winSizeX = 1080
 		self._winSizeY = 1920
 		self._sizeX = x
 		self._sizeY = y
 		self._spriteSize = 100
 		self._scale = 1
-		self._map = [[[{}]]]
+		self._maxItemPerCase = 100
+		self._map = [[{}]]
 		self.buildWindow()
 		self.buildItem()
 		self._mapContent = [[{}]]
@@ -31,19 +31,26 @@ class Drawer():
 			self._map.append([])
 			for x in range(self._sizeX):
 				self._map[y].append([])
-				self.buildCaseItem(self._mapContent[y][x], x, y)
+				self._map[y][x] = {
+					"linemate":[],
+					"deraumere":[],						"sibur":[],
+					"mendiane":[],
+					"phiras":[],
+					"thystame":[],
+					"food":[]
+				}
+				for key, _ in self._map[y][x].items():
+					self._map[y][x][key] = self.buildCaseItem(key, x, y)
 	
-	def buildCaseItem(self, case, x, y):
+	def buildCaseItem(self, key, x, y):
 		tmp = []
-		dic = {} 
-		for key, value in case.items():
-			for i in range(value):
-				dic["name"] = key
-				dic["sprite"] = self._items[key]
-				dic["x"] = (x * self._spriteSize * self._scale) + random.randint(0, self._spriteSize * self._scale) + self._shiftX
-				dic["y"] = (y * self._spriteSize * self._scale) + random.randint(0, self._spriteSize * self._scale) + self._shiftY
-				self._map[y][x].append(dic)
-				i = i
+		for _ in range(self._maxItemPerCase):
+			dic = {}
+			dic["name"] = key
+			dic["sprite"] = self._items[key]
+			dic["x"] = (x * self._spriteSize * self._scale) + random.randint(0, (self._spriteSize * 0.7) * self._scale) + self._shiftX
+			dic["y"] = (y * self._spriteSize * self._scale) + random.randint(0, (self._spriteSize * 0.7) * self._scale) + self._shiftY
+			tmp.append(dic)
 		return tmp
 	
 	def buildWindow(self):
@@ -55,14 +62,14 @@ class Drawer():
 		
 	def buildItem(self):
 		self._items = {}
-		self._items["case"] = pygame.image.load("case.jpg").convert()
-		self._items["food"] = pygame.image.load("items/food.png").convert()
-		self._items["linemate"] =  pygame.image.load("items/linemate.png").convert()
-		self._items["deraumere"] =  pygame.image.load("items/deraumere.png").convert()
-		self._items["sibur"] =  pygame.image.load("items/sibur.png").convert()
-		self._items["mendiane"] =  pygame.image.load("items/mendiane.png").convert()
-		self._items["phiras"] =  pygame.image.load("items/phiras.png").convert()
-		self._items["thystame"] =  pygame.image.load("items/thystame.png").convert()
+		self._items["case"] = pygame.image.load("ground.png").convert_alpha()
+		self._items["food"] = pygame.image.load("items/food.png").convert_alpha()
+		self._items["linemate"] =  pygame.image.load("items/linemate.png").convert_alpha()
+		self._items["deraumere"] =  pygame.image.load("items/deraumere.png").convert_alpha()
+		self._items["sibur"] =  pygame.image.load("items/sibur.png").convert_alpha()
+		self._items["mendiane"] =  pygame.image.load("items/mendiane.png").convert_alpha()
+		self._items["phiras"] =  pygame.image.load("items/phiras.png").convert_alpha()
+		self._items["thystame"] =  pygame.image.load("items/thystame.png").convert_alpha()
 	
 	def start(self):
 		i = 0
@@ -75,6 +82,7 @@ class Drawer():
 	def drawMap(self):
 		for y in range(self._sizeY):
 			for x in range(self._sizeX):
+				print(str(y) + " "+ str(x))
 				self.drawCase(self._map[y][x], self._mapContent[y][x], x, y)
 
 	def drawCase(self, case, caseContent, x, y):
@@ -84,15 +92,14 @@ class Drawer():
 		self.fillCase(case, caseContent)
 
 	def fillCase(self, case, caseContent):
-		for key, value in caseContent:
-			i = 0
-			for el in case:
-				if (i == value):
-					break
-				if (el["name"] == key):
-					i = i + 1
-					self._window.blit(el["sprite"], (el["x"], el["y"]))
+		for key, value in caseContent.items():
+			for unvalue in range(value):
+				self._window.blit(
+					case[key][unvalue]["sprite"],
+					(case[key][unvalue]["x"],
+					case[key][unvalue]["y"])
+				)
 
 if __name__ == '__main__':
-	draw = Drawer(5, 5)
+	draw = Drawer(2, 2)
 	draw.start()
