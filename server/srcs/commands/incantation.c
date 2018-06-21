fd08cd0a853cce263b5b98f7e8f167632f9829bf
Hugo MARTIN <hugo.martin@epitech.eu>
2018-06-21 12:45:18 +0000

    Continue

--- server/srcs/commands/incantation.c
+++ server/srcs/commands/incantation.c
@@ -28,15 +28,19 @@ static int	compare_tile_incantation(tile_t *tile, tile_t level)
 	tile->sibur >= level.sibur &&
 	tile->mendiane >= level.mendiane &&
 	tile->phiras >= level.phiras &&
-	i >= level.minPlayers) {
-		printf("Goodplayer\n");
+	i >= level.minPlayers)
 		return (1);
-	}
 	return (0);
 }
 
 static void	remove_minerals(tile_t **p_tile, tile_t level)
 {
+	printf("%d|%d\n", (*p_tile)->linemate, level.linemate);
+	printf("%d|%d\n", (*p_tile)->deraumere, level.deraumere);
+	printf("%d|%d\n", (*p_tile)->sibur, level.sibur);
+	printf("%d|%d\n", (*p_tile)->mendiane, level.mendiane);
+	printf("%d|%d\n", (*p_tile)->phiras, level.phiras);
+	printf("%d|%d\n", (*p_tile)->thystam, level.thystam);
 	(*p_tile)->linemate -= level.linemate;
 	(*p_tile)->deraumere -= level.deraumere;
 	(*p_tile)->sibur -= level.sibur;
@@ -47,9 +51,10 @@ static void	remove_minerals(tile_t **p_tile, tile_t level)
 
 int	start_incantation(server_t *server, client_t *client, char *str)
 {
-	if (client->level >= 8 || compare_tile_incantation(server->map[client->posY][client->posX], level_requirement[client->level + 1]) == 0)
+	draw_tile(server->map, 1, client->posY, client->posX);
+	if (client->level >= 8 || compare_tile_incantation(server->map[client->posY][client->posX], level_requirement[client->level - 1]) == 0)
 		return KO;
-	remove_minerals(&server->map[client->posY][client->posX], level_requirement[client->level + 1]);
+	remove_minerals(&server->map[client->posY][client->posX], level_requirement[client->level - 1]);
 	draw_tile(server->map, 1, client->posY, client->posX);
 	return OK;
 }
