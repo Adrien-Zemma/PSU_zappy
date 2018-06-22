b2f9144326bab1783fe2eb1387da2cbc3a3ab876
Yohan <yohan.jabin@epitech.eu>
2018-06-22 20:23:19 +0200

    FIX LOOK JPP

--- server/srcs/commands/look.c
+++ server/srcs/commands/look.c
@@ -20,29 +20,14 @@ void	check_look_north(client_t *client,
 	int *nb, int pos_y, server_t *server)
 {
 	int	pos_x = client->pos_x - 1;
-	int	check;
+	int	tmp;
 	int	i = 0;
 
 	do {
-		pos_x++;
-		if (pos_x - nb[0] < 0) {
-			check = pos_x - nb[0];
-			pos_x = server->parse->width - check;
-			check_map(server->map[map_val_pos(server->parse->height,
-				pos_y)][map_val_pos(server->parse->width, pos_x)],
-				client);
-		}
-		else if (pos_x - nb[0] > (server->parse->width - 1)){
-			pos_x = (pos_x) - server->parse->width;
-			check_map(server->map[map_val_pos(server->parse->height,
-				pos_y)][map_val_pos(server->parse->width,
-				pos_x - nb[0])], client);
-		}
-		else  {
-			check_map(server->map[map_val_pos(server->parse->height,
-				pos_y)][map_val_pos(server->parse->width,
-				pos_x - nb[0])], client);
-		}
+		pos_y = map_val_pos(server->parse->height, pos_y);
+		pos_x = map_val_pos(server->parse->width, pos_x + 1);
+		tmp = map_val_pos(server->parse->width, pos_x - nb[0]);
+		check_map(server->map[pos_y][tmp], client);
 	} while (i++ - nb[0] < 1 + nb[0] - 1);
 }
 
@@ -51,14 +36,10 @@ int	look_north(server_t *server, client_t *client, int *nb)
 	int	pos_y;
 
 	dprintf(client->fd, "[");
-
 	pos_y = client->pos_y;
 	for (int i = 0; i < client->level + 1; i++) {
 		check_look_north(client, nb, pos_y, server);
-		if (pos_y - 1 < 0)
-			pos_y = server->parse->height - 1;
-		else
-			pos_y--;
+		pos_y = map_val_pos(server->parse->height, pos_y - 1);
 		nb[0]++;
 		nb[1]++;
 	}
--- server/srcs/commands/look_east.c
+++ server/srcs/commands/look_east.c
@@ -9,45 +9,36 @@
 
 void	check_map(tile_t *map, client_t *client)
 {
-	for (size_t i = 0; i < map->linemate; i++)
-		dprintf(client->fd, "linemate ");
-	for (size_t i = 0; i < map->deraumere; i++)
-		dprintf(client->fd, "deraumere ");
-	for (size_t i = 0; i < map->sibur; i++)
-		dprintf(client->fd, "sibur ");
-	for (size_t i = 0; i < map->mendiane; i++)
-		dprintf(client->fd, "mendiane ");
-	for (size_t i = 0; i < map->phiras; i++)
-		dprintf(client->fd, "phiras ");
-	for (size_t i = 0; i < map->thystam; i++)
-		dprintf(client->fd, "thystam ");
 	for (int i = 0; map->clients[i] != NULL; i++)
 		dprintf(client->fd, "player ");
-	for (size_t i = 0; i < map->food; i++)
+	for (int i = 0; i < map->food; i++)
 		dprintf(client->fd, "food ");
+	for (int i = 0; i < map->linemate; i++)
+		dprintf(client->fd, "linemate ");
+	for (int i = 0; i < map->deraumere; i++)
+		dprintf(client->fd, "deraumere ");
+	for (int i = 0; i < map->sibur; i++)
+		dprintf(client->fd, "sibur ");
+	for (int i = 0; i < map->mendiane; i++)
+		dprintf(client->fd, "mendiane ");
+	for (int i = 0; i < map->phiras; i++)
+		dprintf(client->fd, "phiras ");
+	for (int i = 0; i < map->thystam; i++)
+		dprintf(client->fd, "thystam ");
 	dprintf(client->fd, ",");
 }
 
 void	check_look_east(client_t *client, int *nb, int pos_x, server_t *server)
 {
 	int	pos_y = client->pos_y - 1;
-	int	check;
+	int	tmp;
 	int	i = 0;
 
 	do {
-		pos_y++;
-		if (pos_y - nb[0] < 0) {
-			check = pos_y - nb[0];
-			pos_y = server->parse->height - check;
-			check_map(server->map[map_val_pos(server->parse->height, pos_y)][map_val_pos(server->parse->width, pos_x)], client);
-		}
-		else if (pos_y - nb[0] > (server->parse->height - 1)) {
-			pos_y = pos_y - server->parse->height;
-			check_map(server->map[map_val_pos(server->parse->height, pos_y - nb[0])][map_val_pos(server->parse->width, pos_x)], client);
-		}
-		else {
-			check_map(server->map[map_val_pos(server->parse->height, pos_y - nb[0])][map_val_pos(server->parse->width, pos_x)], client);
-		}
+		pos_y = map_val_pos(server->parse->height, pos_y + 1);
+		pos_x = map_val_pos(server->parse->width, pos_x);
+		tmp = map_val_pos(server->parse->height, pos_y + nb[0]);
+		check_map(server->map[tmp][pos_x], client);
 	} while (i++ - nb[0] < 1 + nb[0] - 1);
 }
 
@@ -59,10 +50,7 @@ int	look_east(server_t *server, client_t *client, int *nb)
 	pos_x = client->pos_x;
 	for (int i = 0; i < client->level + 1; i++) {
 		check_look_east(client, nb, pos_x, server);
-		if (pos_x + 1 > server->parse->width - 1)
-			pos_x = 0;
-		else
-			pos_x++;
+		pos_x = map_val_pos(server->parse->width, pos_x + 1);
 		nb[0]++;
 		nb[1]++;
 	}
